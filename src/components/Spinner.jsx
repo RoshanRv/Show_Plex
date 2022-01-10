a6a1df17c7624bb5f90a2a269864949a06dc9215
Roshan <nkroshankumar@gmail.com>
2022-01-10 10:46:10 +0530

    changes

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div className="w-10 h-10 mx-auto rounded-full border-4 border-gray-300 border-t-rose-700  my-8 animate-spin">
+        <div className="w-10 h-10 mx-auto rounded-full border-4 border-white border-t-rose-700  my-8 animate-spin">
         </div>
     )
 }
